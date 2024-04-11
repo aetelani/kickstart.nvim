06643c9392889d786dfcec1aaaafdede9db5760b
Anssi Eteläniemi <anssi.etelaniemi@confidex.com>
2024-04-11 15:36:24 +0300

    Added rustacean config

--- init.lua
+++ init.lua
@@ -828,7 +828,13 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-
+  -- AE Add own plugins in the end to avoid dependencies issues
+  {
+    -- Remove Mason Rust LSP due conflicts
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
